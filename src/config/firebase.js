0c39afe746f855a3ffe5511a33a16cfad48758fa
Captaintoy <abdulsalamkorede64@gmail.com>
2025-09-23 14:23:39 -0700

    env commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,8 +7,17 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccountPath = path.join(__dirname, "./YMSDB.json");
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Load from .env
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+} else {
+  // Fallback: load from JSON file
+  const serviceAccountPath = path.join(__dirname, "./YMSDB.json");
+  serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
