072342ad431354135e613beabc71c4793f7c6a56
Captaintoy <abdulsalamkorede64@gmail.com>
2025-09-24 19:23:13 -0700

    last commit 1a2b3c4d5e6f7g8h9i0jklmnopqrstuvwx

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -15,12 +15,14 @@ let serviceAccount;
 
 
 if (process.env.NODE_ENV==="production") {
+  console.log(process.env.FIREBASE_SERVICE_ACCOUNT);
+  
   // Load from .env
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 } else {
   // Fallback: load from JSON file
-  const serviceAccountPath = path.join(__dirname, "./YMSDB.json");
+  const serviceAccountPath = path.join(__dirname, "./YMSchool.json");
   serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
 }
 admin.initializeApp({
