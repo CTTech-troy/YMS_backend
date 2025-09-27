2c80e0d556c33bfd488cd2891dcfe8265b87f073
Captaintoy <abdulsalamkorede64@gmail.com>
2025-09-27 14:58:23 -0700

    Testing bugfix for firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -14,17 +14,15 @@ const __dirname = path.dirname(__filename);
 let serviceAccount;
 
 
-if (process.env.NODE_ENV==="production") {
-  console.log(process.env.FIREBASE_SERVICE_ACCOUNT);
-  
+if (process.env.NODE_ENV==="production") {  
   // Load from .env
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 } else {
   // Fallback: load from JSON file
   const serviceAccountPath = path.join(__dirname, "./YMSchool.json");
   serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
 }
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -33,4 +31,3 @@ const db = admin.firestore();
 const auth = admin.auth();
 
 export { admin, db, auth };
- 
